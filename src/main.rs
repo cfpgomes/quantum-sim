75885b7449430f5f1a7e64637600500030c99235
Cláudio Gomes <cfpgcp3@gmail.com>
2020-06-01 01:04:59 +0100

    All gates implemented but Toffoli

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,48 @@ enum Gate {
     CY { c: u64, t: u64 },
     CZ { c: u64, t: u64 },
     SWAP { c: u64, t: u64 },
+    CCX { c0: u64, c1: u64, t: u64 },
+}
+
+fn controlled_gate(c: u64, t: u64, u: &na::DMatrix<Complex64>) -> na::DMatrix<Complex64> {
+    let mut c0part = na::DMatrix::identity_generic(na::Dynamic::new(1), na::Dynamic::new(1));
+    let mut c1part = na::DMatrix::identity_generic(na::Dynamic::new(1), na::Dynamic::new(1));
+    for q in na::min(c, t)..=na::max(c, t) {
+        if q == c {
+            c0part = c0part.kronecker(&na::DMatrix::from_vec_generic(
+                na::Dynamic::new(2),
+                na::Dynamic::new(2),
+                vec![
+                    Complex64::new(1., 0.),
+                    Complex64::new(0., 0.),
+                    Complex64::new(0., 0.),
+                    Complex64::new(0., 0.),
+                ],
+            ));
+            c1part = c1part.kronecker(&na::DMatrix::from_vec_generic(
+                na::Dynamic::new(2),
+                na::Dynamic::new(2),
+                vec![
+                    Complex64::new(0., 0.),
+                    Complex64::new(0., 0.),
+                    Complex64::new(0., 0.),
+                    Complex64::new(1., 0.),
+                ],
+            ));
+        } else if q == t {
+            c0part = c0part.kronecker(&Gate::I { t }.matrix());
+            c1part = c1part.kronecker(u);
+        } else {
+            c0part = c0part.kronecker(&Gate::I { t }.matrix());
+            c1part = c1part.kronecker(&Gate::I { t }.matrix());
+        }
+    }
+    c0part + c1part
 }
 
 impl Gate {
     fn matrix(&self) -> na::DMatrix<Complex64> {
-        match self {
+        match *self {
             Gate::I { t: _ } => {
                 na::DMatrix::identity_generic(na::Dynamic::new(2), na::Dynamic::new(2))
             }
@@ -84,17 +121,43 @@ impl Gate {
                     Complex64::from_polar(&1., &na::RealField::frac_pi_4()),
                 ],
             ),
-            Gate::CX { c: _, t: _ } => unimplemented!(),
-            Gate::CY { c: _, t: _ } => unimplemented!(),
-            Gate::CZ { c: _, t: _ } => unimplemented!(),
-            Gate::SWAP { c: _, t: _ } => unimplemented!(),
+            Gate::CX { c, t } => controlled_gate(c, t, &Gate::X { t }.matrix()),
+            Gate::CY { c, t } => controlled_gate(c, t, &Gate::Y { t }.matrix()),
+            Gate::CZ { c, t } => controlled_gate(c, t, &Gate::Z { t }.matrix()),
+            Gate::SWAP { c, t } => {
+                Gate::CX { c: c, t: t }.matrix()
+                    * Gate::CX { c: t, t: c }.matrix()
+                    * Gate::CX { c: c, t: t }.matrix()
+            }
+            Gate::CCX { c0, c1, t } => unimplemented!(),
         }
     }
 }
 
 fn main() {
     let gate_i = Gate::I { t: 1 };
+    let gate_x = Gate::X { t: 1 };
+    let gate_y = Gate::Y { t: 1 };
+    let gate_z = Gate::Z { t: 1 };
+    let gate_h = Gate::H { t: 1 };
+    let gate_s = Gate::S { t: 1 };
     let gate_t = Gate::T { t: 1 };
+    let gate_cx = Gate::CX { c: 1, t: 2 };
+    let gate_cy = Gate::CY { c: 1, t: 2 };
+    let gate_cz = Gate::CZ { c: 1, t: 2 };
+    let gate_swap = Gate::SWAP { c: 1, t: 2 };
+    let gate_ccx = Gate::CCX { c0: 1, c1: 2, t: 3 };
 
-    print!("{}", gate_t.matrix());
+    print!("Identity Gate\n{}", gate_i.matrix());
+    print!("Pauli-X Gate\n{}", gate_x.matrix());
+    print!("Pauli-Y Gate\n{}", gate_y.matrix());
+    print!("Pauli-Z Gate\n{}", gate_z.matrix());
+    print!("Hadamard Gate\n{}", gate_h.matrix());
+    print!("Phase Gate\n{}", gate_s.matrix());
+    print!("π/8 Gate\n{}", gate_t.matrix());
+    print!("Controlled X Gate\n{}", gate_cx.matrix());
+    print!("Controlled Y Gate\n{}", gate_cy.matrix());
+    print!("Controlled Z Gate\n{}", gate_cz.matrix());
+    print!("Swap Gate\n{}", gate_swap.matrix());
+    print!("Toffoli Gate\n{}", gate_ccx.matrix());
 }
